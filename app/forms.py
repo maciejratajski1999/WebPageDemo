3d4b5bc43bba71bbe17d55bc03763f5911ea8c83
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-08-25 12:09:46 +0200

    add files: blog.html blog.css
    changed routes
    reseted database

---
+++ app/forms.py
@@ -0,0 +1,98 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, field):
+        username_taken = User.query.filter_by(username=field.data).first()
+        if username_taken:
+            raise ValidationError("username already taken")
+
+    def validate_email(self, field):
+        email_taken = User.query.filter_by(email=field.data).first()
+        if email_taken:
+            raise ValidationError("email already taken")
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',
+                        validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class PictureForm(FlaskForm):
+    product_id = IntegerField('Product ID', validators=[DataRequired()], default=0)
+    picture = FileField(validators=[FileAllowed(['jpg', 'jpeg', 'png']), DataRequired()])
+    title = StringField('Title', validators=[DataRequired()])
+    submitpicture = SubmitField('Add new Picture')
+
+def picture_form_id(product_id):
+
+    class PictureFormID(PictureForm):
+        pass
+
+    PictureFormID.product_id = IntegerField('Product ID', validators=[DataRequired()], default=product_id)
+    PictureFormID.picture = FileField(validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    PictureFormID.title = StringField('Title', validators=[DataRequired()])
+    PictureFormID.submitpicture = SubmitField('Add new Picture')
+
+    return PictureFormID()
+
+
+
+class ProductForm(FlaskForm):
+    name = StringField("Product Name", validators=[DataRequired()])
+    thumbnail = FileField(validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    submitproduct = SubmitField('Add new Product')
+
+class ApplyChangesForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Apply Changes')
+
+class DeleteProductForm(FlaskForm):
+    product_id = IntegerField('Product ID', validators=[DataRequired()], default=0)
+    confirm = BooleanField('Delete this product', validators=[EqualTo(1)])
+    delete_product = SubmitField('Delete product')
+
+def delete_product_id(product_id):
+
+    class DeleteProductID(DeleteProductForm):
+        pass
+
+    DeleteProductID.product_id = IntegerField('Product ID', validators=[DataRequired()], default=product_id)
+    DeleteProductID.delete_product = SubmitField('Delete product')
+
+    return DeleteProductID()
+
+class DeletePictureForm(FlaskForm):
+    picture_path = StringField("Product Name", validators=[DataRequired()], default='')
+    submit = SubmitField('Delete picture')
+
+def delete_picture_form(path):
+
+    class DeletePicture(DeletePictureForm):
+        pass
+
+    DeletePicture.picture_path = StringField("Picture path", validators=[DataRequired()], default=path)
+
+    return DeletePicture()
+
+class BlogPostForm(FlaskForm):
+
+    author = StringField("Author", validators=[DataRequired(), Length(min=2, max=32)], default='admin')
+    content = TextAreaField('Content', validators=[DataRequired(), Length(min=1, max=32)])
+    submit = SubmitField('Create new post')
---
+++ app/routes.py
@@ -0,0 +1,130 @@
+from flask import render_template, flash, redirect, url_for, send_from_directory, request
+from app.forms import *
+from app.utils import *
+from flask_login import login_user, logout_user, current_user
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    current = 'home'
+    return render_template('home.html', subpages=subpages, current=current)
+
+
+@app.route("/about")
+def about():
+    current = 'about'
+    return render_template('about.html', subpages=subpages, current=current)
+
+
+@app.route("/gallery", methods=['GET', 'POST'])
+def gallery():
+    current = 'gallery'
+    products = Product.query.all()
+    products = [[product.id, product.name, product.thumbnail] for product in products]
+    products = split_into_groups_of_n(objects=products, n=3)
+    return render_template('gallery.html', subpages=subpages, current=current, products=products)
+
+
+@app.route("/products", methods=['GET'])
+def products():
+    current = 'products'
+    products = get_products_in_group_of_n(3)
+    return render_template('products.html', subpages=subpages, current=current, products=products)
+
+
+@app.route("/product", methods=['GET', 'POST'])
+def product():
+    current = "products"
+    product_id = int(request.args.get('product_id'))
+    product = Product.query.get(product_id)
+    if product:
+        pictures = product.pictures
+        pictures = [[picture.path, picture.title, delete_picture_form(picture.path)] for picture in pictures]
+        pictures_split = split_into_groups_of_n(pictures, n=3)
+        delete_picture_forms = [picture[2] for picture in pictures]
+        for form in delete_picture_forms:
+            if form.validate_on_submit():
+                if form.picture_path.data:
+                    pic_to_delete = get_picture(form.picture_path.data)
+                    title = pic_to_delete.title
+                    delete_picture(pic_to_delete)
+                    flash(f'Deleted a picture: {title}', 'alert')
+                    return redirect(url_for('product', product_id=product_id))
+        return render_template('product.html', subpages=subpages, current=current, pictures=pictures_split)
+    else:
+        return redirect('products')
+
+
+@app.route("/manage", methods=['GET', 'POST'])
+def manage():
+    if current_user.is_authenticated:
+        product_form = ProductForm()
+        products = Product.query.all()
+        products = [product for product in products]
+        picture_forms = {product.id : picture_form_id(product_id=product.id) for product in products}
+        if product_form.validate_on_submit():
+            if product_form.thumbnail.data:
+                add_product(product_form)
+                return redirect('manage')
+        for picture_form in picture_forms.values():
+            if picture_form.validate_on_submit():
+                if picture_form.picture.data:
+                    add_picture(picture_form)
+                    return redirect(url_for('manage'))
+        apply_changes_form = ApplyChangesForm()
+        if apply_changes_form.validate_on_submit():
+            if verify_password(current_user, apply_changes_form.password.data):
+                new_images_count = len(save_images())
+                flash(f"success, {new_images_count} new images saved in database", 'alert')
+            else:
+                flash(f"Wrong password for {current_user.username}", 'alert')
+        delete_product_forms = {product.id : delete_product_id(product.id) for product in products}
+        for delete_form in delete_product_forms.values():
+            if delete_form.validate_on_submit():
+                if delete_form.confirm.data:
+                    delete_product_by_id(delete_form.product_id.data)
+                    return redirect(url_for('manage'))
+
+        return render_template('manage.html', subpages=subpages, products=products, product_form=product_form, picture_forms=picture_forms, apply_changes_form=apply_changes_form, delete_forms=delete_product_forms)
+    return render_template('home.html', subpages=subpages)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        flash(f"Already logged in as {current_user.username}", 'alert')
+        return redirect('home')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash("Login unsuccessful", 'alert')
+    return render_template('login.html', subpages=subpages, form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'tux.png', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/blog', methods=['GET', 'POST'])
+def blog():
+    if current_user.is_authenticated:
+        form = BlogPostForm()
+        return render_template('blog.html', subpages=subpages, current='blog', form=form)
+    return render_template('blog.html', subpages=subpages, current='blog')
+
+@app.route('/register', methods=['GET', 'POST'])
+def reigster():
+    form = RegistrationForm()
+    return render_template('register.html', subpages=subpages, form=form)
