22f2ef75e89a3f0df4d9063594d3b0c156015ba6
Maciej Ratajski <maciejratajski1999@gmail.com>
2021-09-17 16:20:15 +0200

    added additional launcher for debug mode moved bcrypt and loginmanager to utils

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 from app.create_app import create_app
-from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
+
 
 app = create_app()
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+
 from app.models import db
 
 from app import routes
--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,15 @@
-from app import app, db, bcrypt, login_manager
+from app import app
 from app.models import *
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 import string
 import random
 from PIL import Image as PilImage
 from datetime import datetime
 
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 allowed_characters = string.ascii_letters + string.digits
 subpages = {'home': {"Home": "/"}, 'about': {"About": "/about"}, 'products': {"Products": "/products"},
             'blog': {'Blog': '/blog'}}
